25a3676f79b40b22a86a4e5937132344810932f3
danding <dandingkeji.top@gmail.com>
2024-07-16 12:36:42 +0800

    #qqq

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     remix({
       future: {
