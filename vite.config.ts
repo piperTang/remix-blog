3d9017fc80ac0a468e4611cd2a993bb44735a82d
danding <dandingkeji.top@gmail.com>
2024-07-16 15:42:38 +0800

    #1111

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset } from "@vercel/remix/vite";
 
 export default defineConfig({
   // define: {
@@ -13,8 +12,7 @@ export default defineConfig({
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-      },
-      presets: [vercelPreset()],
+      }
     }),
     tsconfigPaths(),
   ],
